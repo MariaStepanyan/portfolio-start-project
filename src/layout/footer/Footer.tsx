70acc0ac54926e2e390c12c6a6a7c39ad20b690e
MariaStepanyan <drmariastepanyan@gmail.com>
2023-12-23 16:42:59 +0300

    adaptive mobile footer & min-width for body

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { Icon } from '../../components/icon/Icon'
 import { FlexWrapper } from '../../components/FlexWrapper'
 import { theme } from '../../styles/Theme'
+import { font } from '../../styles/common'
 
 export const Footer = () => {
   return (
@@ -62,9 +63,12 @@ const StyledFooter = styled.footer`
 `
 
 const Name = styled.span`
-  font-family: 'Josefin Sans', sans-serif;
-  font-size: 22px;
-  font-weight: 700;
+  ${font({
+    family: "'Josefin Sans', sans-serif",
+    weight: 700,
+    Fmax: 22,
+    Fmin: 16,
+  })}
   letter-spacing: 3px;
 `
 
