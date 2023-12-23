ed031a15dfa824527a2817b00b0c4db1430ac8c6
MariaStepanyan <drmariastepanyan@gmail.com>
2023-12-23 23:48:13 +0300

    refacter Works

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -3,7 +3,7 @@ import { SectionTitle } from '../../../components/SectionTitle'
 import { Icon } from '../../../components/icon/Icon'
 import { Slider } from '../../../components/slider/Slider'
 import { FlexWrapper } from '../../../components/FlexWrapper'
-import { IconWrapper } from '../skills/skill/Skill'
+import  { S } from '../skills/Skills_Styles'
 import { Container } from '../../../components/Container'
 
 export const Testimony = () => {
@@ -12,9 +12,9 @@ export const Testimony = () => {
       <Container>
         <SectionTitle>Testimony</SectionTitle>
         <FlexWrapper direction="column" align="center">
-          <IconWrapper>
+          <S.IconWrapper>
             <Icon iconId="quote" />
-          </IconWrapper>
+          </S.IconWrapper>
           <Slider />
         </FlexWrapper>
       </Container>
@@ -26,7 +26,7 @@ const StyledTestimony = styled.section`
   background-color: aliceblue;
   min-height: 50vh;
 
-  ${IconWrapper} {
+  ${S.IconWrapper} {
     margin: 28px 0 72px;
   }
 `
--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,4 +1,3 @@
-import styled from 'styled-components'
 import { SectionTitle } from '../../../components/SectionTitle'
 import { TabMenu } from './menu/TabMenu'
 import { FlexWrapper } from '../../../components/FlexWrapper'
@@ -6,34 +5,37 @@ import { Work } from './work/Work'
 import socialImgPng from '../../../assets/images/proj1.png'
 import timerImgPng from '../../../assets/images/proj2.png'
 import { Container } from '../../../components/Container'
+import { S } from './Works_Styles'
 
 const worksItems = ['All', 'landing page', 'React', 'spa']
 
-export const Works = () => {
+const workdata = [
+  {
+    src: socialImgPng,
+    title: 'Social Network',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  },
+  {
+    src: timerImgPng,
+    title: 'Timer',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+  },
+]
+
+export const Works: React.FC = () => {
   return (
-    <StyledWorks>
+    <S.Works>
       <Container>
         <SectionTitle>My Works</SectionTitle>
         <TabMenu menuItem={worksItems} />
-        <FlexWrapper justify="space-between" align="flex-start" wrap='wrap'>
-          <Work
-            src={socialImgPng}
-            title="Social Network"
-            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-          />
-          <Work
-            src={timerImgPng}
-            title="Timer"
-            text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"
-          />
+        <FlexWrapper justify="space-between" align="flex-start" wrap="wrap">
+          {workdata.map((w, item) => {
+            return <Work src={w.src} key={item} title={w.title} text={w.text} />
+          })}
         </FlexWrapper>
       </Container>
-    </StyledWorks>
+    </S.Works>
   )
 }
 
-const StyledWorks = styled.section`
-${FlexWrapper} {
-  gap: 30px;
-}
-`
+
